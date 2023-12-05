286f29bf5d661e41077caaf5a72df772721229f2
Javiera Vukasovic <javieravukasovic@gmail.com>
2023-12-04 22:22:59 -0300

    feat: create query to populate boulder info

--- client/src/pages/detailBoulder.page/detailBoulder.page.jsx
+++ client/src/pages/detailBoulder.page/detailBoulder.page.jsx
@@ -1,44 +1,11 @@
 
 import {List, Flex, Rate, Button} from 'antd';
-
 import { Wrapper} from "@googlemaps/react-wrapper";
-
-import React, { useEffect, useRef } from "react";
-import { useNavigate } from 'react-router-dom';
-
-
-
+import React, { useEffect, useRef, useState } from "react";
+import { useNavigate, useParams } from 'react-router-dom';
+import axios from 'axios';
 
 const apikey = process.env.REACT_APP_MAPS_API_KEY
-
-const data = {
-        "boulderName": "prueba3",
-        "grade": "V32",
-        "description": "prueba 1234",
-        "geolocation": {
-            "type": "Point",
-            "coordinates": [
-                1,
-                2
-            ]
-        },
-        "calification": [
-            {
-                "rating": 5,
-                "comment": "muy bueno",
-                "_id": "656dfa01d3e24590e63689eb"
-            },
-            {
-                "rating": 2,
-                "comment": "maleno",
-                "_id": "656dfa01d3e24590e63689ec"
-            }
-        ],
-        "_id": "656dfa01d3e24590e63689ea",
-        "createdAt": "2023-12-04T16:10:41.511Z",
-        "updatedAt": "2023-12-04T16:10:41.511Z",
-        "__v": 0
-    };
 
 function MyMapComponent({
   center,
@@ -63,120 +30,159 @@ function MyMapComponent({
 }
 
 const DetailBoulderPage = () => {
-  const render = (status) => {
-    return <h1>{status}</h1>;
-  };
-  const center = { lat: -34.397, lng: 150.644 };
-  const zoom = 4;
+    const [boulderName, setBoulderName] = useState('');
+    const [grade, setGrade] = useState('');
+    const [meanRating, setMeanRating] = useState();
+    const [description, setDescription] = useState('');
+    const [reviews, setReviews] = useState([]);
 
-  const navigate = useNavigate();
+    const render = (status) => {
+        return <h1>{status}</h1>;
+    };
+    const center = { lat: -34.397, lng: 150.644 };
+    const zoom = 4;
 
-  return (
-    <>
-    <Flex justify={'space-between'} align={'flex-start'} wrap='wrap'>
-        <div>
-            <div 
-                style={{
-                        marginBottom:8
-                    }}
-            >
-                
-                <h1 
+    const params = useParams();
+    const navigate = useNavigate();
+
+    const idBoulder = params.idBoulder
+
+    const getBoulderData = async () => {
+        try {
+            const getOneResult = await axios.get("http://localhost:8000/api/boulders/getOne/" + idBoulder);
+            setBoulderName(getOneResult.data.boulderName);
+            setGrade(getOneResult.data.grade);
+            setMeanRating(getOneResult.data.meanRating);
+            setDescription(getOneResult.data.description);
+            setReviews(getOneResult.data.calification);
+        } catch (e) {
+            alert(e.response.data.message);
+        }
+    }
+
+    const handleDelete = async (idBoulder) => {
+        try {
+            const deleteResult = await axios.delete("http://localhost:8000/api/boulders/delete/" + idBoulder);
+            if(deleteResult.status == 200){
+                //var listAuthorsTemp = listAuthors.filter((auth) => auth._id != idAuth);
+                alert("Deleted correctly");
+                navigate('/boulders');
+            } else {
+                alert("Error");
+            }
+        } catch (e) {
+            
+        }
+    }
+
+    useEffect(() => {
+        getBoulderData();
+    }, []);  
+
+    return (
+        <Flex justify={'space-between'} align={'flex-start'} wrap='wrap'>
+            <div>
+                <div style={{marginBottom:8}}> 
+                    <h1 style={{display:'inline', marginRight:16}} >
+                        {boulderName} 
+                    </h1> 
+
+                    <h2 style={{ display:'inline', color:'#000000A6'}}>
+                        {grade}
+                    </h2>
+                </div>
+
+                <Rate 
+                    disabled 
+                    value={meanRating}
                     style={{
-                        display:'inline',
-                        marginRight:16
-                        }}
-                >
-                    {data.boulderName} 
-                </h1> 
-
-                <h2 
-                    style={{
-                        display:'inline',
-                        color:'#000000A6'
+                        marginTop:8
                     }}
-                >
-                    {data.grade}
+                />
+                <p>{description}</p>
+
+                <h2 style={{
+                marginTop:30
+                }}>
+                    Reviews
                 </h2>
-
+                <List
+                    style={{
+                        maxWidth: 450,
+                        width:450,
+                        verticalAlign:'center'
+                    }}
+                    itemLayout="vertical"
+                    size="small"
+                    dataSource={reviews}
+                    renderItem={(item) => (
+                        <List.Item
+                            extra={
+                                <Rate disabled defaultValue={item.rating} />
+                            }
+                        >
+                        <List.Item.Meta
+                            title={item.userName}
+                        />
+                        
+                        <div>
+                            {item.comment}
+                        </div>
+                        </List.Item>
+                    )}
+                />
             </div>
-
-            <Rate 
-                disabled 
-                defaultValue={0} 
+            
+            <div
                 style={{
-                    margin:8
+                    textAlign: 'center'
                 }}
-            />
-            <p>Calcular promedio de ratings</p> 
-            <p>{data.description}</p>
-
-            <h2 style={{
-            marginTop:30
-            }}>
-                Comentarios
-            </h2>
-            <List
-                style={{
-                    maxWidth: 450,
-                    width:450,
-                }}
-                itemLayout="vertical"
-                size="small"
-                dataSource={data.calification}
-                renderItem={(item) => (
-                    <List.Item
-                        extra={
-                            <Rate disabled defaultValue={item.rating} />
-                        }
+            >
+                <Wrapper 
+                    apiKey={apikey} 
+                    render={render}
+                >
+                    <MyMapComponent center={center} zoom={zoom} />
+                </Wrapper>
+                <Button
+                    type="primary"
+                    style={{
+                        margin:8
+                    }}
+                >
+                    Add review
+                </Button>
+                <Button
+                    type="primary"
+                    onClick={()=>navigate('/boulders')}
+                    style={{
+                        margin:8
+                    }}
+                >
+                    Back
+                </Button>
+                <div>
+                    <Button
+                        type="primary"
+                        style={{
+                            margin:8
+                        }}
                     >
-                    <List.Item.Meta
-                        title={item._id}
-                    />
-                    
-                    <div>
-                        {item.comment}
-                    </div>
-                    </List.Item>
-                )}
-            />
-        </div>
-        
-        <div
-            style={{
-                textAlign: 'center'
-            }}
-        >
-            <Wrapper 
-                apiKey={apikey} 
-                render={render}
-            >
-                <MyMapComponent center={center} zoom={zoom} />
-            </Wrapper>
-            <Button
-                type="primary"
-                style={{
-                    margin:8
-                }}
-            >
-                Edit Boulder
-            </Button>
-            <Button
-                type="primary"
-                onClick={()=>navigate('/boulders')}
-                style={{
-                    margin:8
-                }}
-            >
-                Back
-            </Button>
-        </div>
-
-
-    </Flex>
-    
-    </> 
-  );
+                        Edit Boulder
+                    </Button>
+                    <Button
+                        type="primary"
+                        onClick={()=>handleDelete(idBoulder)}
+                        style={{
+                            margin:8
+                        }}
+                    >
+                        Delete
+                    </Button>
+                </div>
+            </div>
+        </Flex>
+  )
 };
 export default DetailBoulderPage;
 
